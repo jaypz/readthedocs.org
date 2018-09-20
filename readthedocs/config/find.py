61934d562067633a7d9d4bcb71286f9b858d550a
John Pease <john.pease@plangrid.com>
2018-09-19 20:31:10 -0400

    handle exception

--- readthedocs/config/find.py
+++ readthedocs/config/find.py
@@ -3,21 +3,24 @@
 from __future__ import division, print_function, unicode_literals
 
 import os
-import re
 
 
-def find_all(path, filename_regex):
-    """Find all files in ``path`` that match ``filename_regex`` regex."""
+def find_all(path, filenames):
+    """Find all files in ``path`` that match in ``filenames``."""
     path = os.path.abspath(path)
-    for root, dirs, files in os.walk(path, topdown=True):
-        dirs.sort()
-        for filename in files:
-            if re.match(filename_regex, filename):
-                yield os.path.abspath(os.path.join(root, filename))
+    try:
+        for root, dirs, files in os.walk(path, topdown=True):
+            dirs.sort()
+            for filename in filenames:
+                if filename in files:
+                    yield os.path.abspath(os.path.join(root, filename))
+    except UnicodeDecodeError:
+        # This is an error of not being able to walk the dir when there are unicode files in them :|
+        pass
 
 
-def find_one(path, filename_regex):
-    """Find the first file in ``path`` that match ``filename_regex`` regex."""
-    for _path in find_all(path, filename_regex):
+def find_one(path, filenames):
+    """Find the first file in ``path`` that match in ``filenames``."""
+    for _path in find_all(path, filenames):
         return _path
     return ''
